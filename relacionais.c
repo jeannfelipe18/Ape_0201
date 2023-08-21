31ee04f0782104ed986639d758429f52f578e77f
jeannfelipe18 <135741976+jeannfelipe18@users.noreply.github.com>
2023-08-21 11:24:32 -0300

    Add files via upload

---
+++ relacionais.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+main(){
+    /*operadores relacionais
+    ==igual
+    > maior que
+    >= maior ou igual
+    < menor
+    <= menor ou igual
+    != diferente
+    em c verdadeiro qualquer numero diferente de 0
+    e o falso é igual a 0.
+    */
+
+   int a = 10, b = 15;
+
+   //estrutura de decisão/seleção
+
+   if(a < b){
+    printf("A é menor\n");
+   }
+   else
+   {
+    printf("B e menor\n");
+   }
+   
+   printf("continua a execucao do codigo");
+}
