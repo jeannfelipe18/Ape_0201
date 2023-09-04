369ccd02eaf18abe39e213e989c89de082a1ce01
jeannfelipe18 <135741976+jeannfelipe18@users.noreply.github.com>
2023-09-04 11:25:15 -0300

    Update repeticao.c

--- repeticao.c
+++ repeticao.c
@@ -5,15 +5,17 @@ int main(){
     //repeticao com condicao no inicio (while)
 
     int numero = 0;
-
-   numero++;
-    printf("\n%d",numero);
-    numero++;
-    printf("\n%d",numero);
-    numero++;
    
-   while(numero < 11){
+  /* while(numero < 11){
         printf("\n%d",2 * numero);
         numero++;
+    }*/
+    /*do{
+        printf("\n%d", 2 * numero);
+        numero++;
+    }while(numero == 100);*/
+
+    for(int contador = 100; contador <= 200; contador++){
+        printf("\n%d", contador);
     }
 }
